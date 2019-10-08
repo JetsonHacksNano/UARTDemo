a1327f597e9acf020aec0f1700c9322f23385ad6
JetsonHacksNano <jim@jetsonhacks.com>
2019-10-08 14:53:38 -0700

    Initial Commit

---
+++ uart_example.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import time
+import serial
+
+print("UART Demonstration Program")
+print("NVIDIA Jetson Nano Developer Kit")
+
+
+serial_port = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+)
+# Wait a second to let the port initialize
+time.sleep(1)
+
+try:
+    # Send a simple header
+    serial_port.write("UART Demonstration Program\r\n".encode())
+    serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
+    while True:
+        if serial_port.inWaiting() > 0:
+            data = serial_port.read()
+            print(data)
+            serial_port.write(data)
+            # if we get a carriage return, add a line feed too
+            # \r is a carriage return; \n is a line feed
+            # This is to help the tty program on the other end 
+            # Windows is \r\n for carriage return, line feed
+            # Macintosh and Linux use \n
+            if data == "\r".encode():
+                # For Windows boxen on the other end
+                serial_port.write("\n".encode())
+
+
+except KeyboardInterrupt:
+    print("Exiting Program")
+
+except Exception as exception_error:
+    print("Error occurred. Exiting Program")
+    print("Error: " + str(exception_error))
+
+finally:
+    serial_port.close()
+    pass
